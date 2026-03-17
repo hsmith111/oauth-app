77bf1e3270a806d129aa83239aba6347a11019c0
Heidi Smith <heidi.smith@ramseysolutions.com>
2026-03-17 15:23:55 -0500

    OAuth changes made

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 /* 
  * Package Imports
 */
+const session = require('express-session');
 
 const path = require("path");
 require("dotenv").config();
@@ -22,12 +23,26 @@ const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
 /*
  * Passport Configurations
 */
+const passport = require('passport');
+const GitHubStrategy = require('passport-github2').Strategy;
 
+passport.use(new GitHubStrategy({
+  clientID: GITHUB_CLIENT_ID,
+  clientSecret: GITHUB_CLIENT_SECRET,
+  callbackURL: "http://localhost:3000/auth/github/callback"
+},
+    (accessToken, refreshToken, profile, done) => {
+      return done(null, profile);
+    }
+));
 
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
 
-
-
-
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
 
 /*
  *  Express Project Setup
@@ -38,9 +53,19 @@ app.set('view engine', 'ejs');
 app.use(partials());
 app.use(express.json());
 app.use(express.static(__dirname + '/public'));
+app.use(session({ secret: 'codecademy', resave: false, saveUninitialized: false }));
+app.use(passport.initialize());
+app.use(passport.session());
 
-
-
+/*
+ * ensureAuthenticated Callback Function
+*/
+const ensureAuthenticated = (req, res, next) => {
+    if (req.isAuthenticated()) {
+      return next();
+    }
+    res.redirect('/login');
+}
 
 /*
  * Routes
@@ -50,7 +75,7 @@ app.get('/', (req, res) => {
   res.render('index', { user: req.user });
 })
 
-app.get('/account', (req, res) => {
+app.get('/account', ensureAuthenticated, (req, res) => {
   res.render('account', { user: req.user });
 });
 
@@ -63,7 +88,13 @@ app.get('/logout', (req, res) => {
   res.redirect('/');
 });
 
+app.get('/auth/github',
+    passport.authenticate('github', {scope: ['user']})
+);
 
+app.get('/auth/github/callback',
+    passport.authenticate('github', { failureRedirect: '/login', successRedirect: '/'})
+);
 
 
 /*
@@ -71,8 +102,3 @@ app.get('/logout', (req, res) => {
 */
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
-
-/*
- * ensureAuthenticated Callback Function
-*/
-
